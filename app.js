144ea03b688ddee3b978d0c31e5de1352b5646da
shazwan <shazwanx23@gmail.com>
2016-11-26 17:17:28 +0800

    using callbacks to abstract logic

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const request = require('request');
 const yargs = require('yargs');
 
+const geocode = require('./geocode/geocode.js');
 const argv = yargs
 	.options({
 		a: {
@@ -14,20 +14,10 @@ const argv = yargs
 	.alias('help', 'h')
 	.argv;
 
-var encoddedAddress = encodeURIComponent(argv.address);
-
-request({
-	url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encoddedAddress}` ,
-	// 1301%20lombard%20street%20philadelphia' ,
-	json: true
-}, (error, response, body) => {
-	 if(error){
-	 	console.log('Unable to connect to Google servers.');
-	 }else if(body.status === 'ZERO_RESULTS'){
-	 	console.log('Unable to find that address.');
-	 }else if(body.status === 'OK'){
-		console.log(`Address: ${body.results[0].formatted_address}`);
-		console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-		console.log(`Latitude: ${body.results[0].geometry.location.lng}`);	 	
-	 }
-})
+geocode.geocodeAddress(argv.address, (errorMessage, results)=> {
+	if(errorMessage){
+		console.log(errorMessage);
+	} else {
+		console.log(JSON.stringify(results, undefined, 2));
+	}
+});
